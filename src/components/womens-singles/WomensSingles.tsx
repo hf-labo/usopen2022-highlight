8d1d3033a2a23df9b2bc9feb2c43e03a8199e385
HFラボ <hf.labo.free@gmail.com>
2022-09-08 15:02:41 +0900

    Singles Round QF追加

--- src/components/womens-singles/WomensSingles.tsx
+++ src/components/womens-singles/WomensSingles.tsx
@@ -7,13 +7,14 @@ import Data1 from './RD1.json'
 import Data2 from './RD2.json'
 import Data3 from './RD3.json'
 import Data4 from './RD4.json'
+import DataQF from './RDQF.json'
 
 const WomensSingles: React.FC = () => {
   const [tab, setTab] = React.useState<string>('0')
   const [data, setData] = React.useState<any>(Data1)
   const handleChangeTab = (event: React.SyntheticEvent, newValue: string) => {
     setTab(newValue)
-    setData([Data1, Data2, Data3, Data4, {"data": []}, {"data": []}, {"data": []}][parseInt(newValue)])
+    setData([Data1, Data2, Data3, Data4, DataQF, {"data": []}, {"data": []}][parseInt(newValue)])
   }
   return (
     <div className="womensSingles">
